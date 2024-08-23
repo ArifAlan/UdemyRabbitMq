aea2b4cae6c84c4d7929bfd95eee5f40e4866655
ArifAlan <91489739+ArifAlan@users.noreply.github.com>
2024-08-23 15:50:28 +0300

    Add project files.

---
+++ UdemyRabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,58 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace UdemyRabbitMQ.Subscriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://bqxqfvzg:gw3_bkaaGY-QlejPK_bAPBgA7nJJ9NDO@codfish.rmq.cloudamqp.com/bqxqfvzg");
+
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            // channel.QueueDeclare("hello-queue", true, false, false);
+
+
+            var randomQueue = "log-database-save";//channel.QueueDeclare().QueueName;
+            channel.QueueDeclare(randomQueue, true, false, false);
+
+            channel.QueueBind(randomQueue, "logs-fanout","",null);
+
+            channel.BasicQos(0,1,false );
+            var consumer=new EventingBasicConsumer(channel);
+            //Kuyruğu dinlemeye başla
+            Console.WriteLine("Loglar dinleniyor.");
+            channel.BasicConsume(randomQueue, false, consumer);
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Thread.Sleep(1500);
+                Console.WriteLine("Mesaj alındı: {0}", message);
+
+                channel.BasicAck(e.DeliveryTag,false);//RabbitMQ'da bir mesajın başarıyla işlendiğini onaylamak için kullanılır. Bu metodun amacı, mesajın işleme alındığını ve kuyruktan kaldırılmasını belirtmektir. Bu, mesajın başarılı bir şekilde işlenip işlenmediğini belirlemek için kullanılır ve mesajın tekrar işlenmeyeceğini garanti eder.
+            };
+
+
+            Console.ReadLine();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+//string queue: "myQueue",        // Dinlenecek kuyruğun adı
+//            autoAck: true,           // Mesajların otomatik olarak alındı olarak işaretlenmesi (true) veya manuel olarak işaretlenmesi (false)
+//            consumerTag: "",         // Bu tüketici için benzersiz etiket. Boş bırakılırsa RabbitMQ otomatik bir etiket oluşturur.
+//            noLocal: false,          // Bu tüketici aynı bağlantı üzerinden gönderilen mesajları almazsa true. Genellikle false olarak ayarlanır.
+//            exclusive: false,        // Kuyruğun sadece bu tüketici tarafından kullanılmasını sağlar (true) veya diğer tüketicilerin de erişebilmesini sağlar (false)
+//            noAck: false,           // Mesajların otomatik olarak alındı olarak işaretlenip işaretlenmeyeceğini belirtir. Genellikle autoAck ile birlikte kullanılır.
+//            consumer: consumer       // Mesajları alacak ve işlemek üzere tanımlanmış EventingBasicConsumer nesnesi
+
+// channel.QueueDeclare("hello-queue", true, false, false);//bir kuyruğu (queue) tanımlamak ve oluşturmak için kullanılan bir yöntemdir.
+//  var consumer=new EventingBasicConsumer(channel);//RabbitMQ'dan mesajları alırken kullanıcının mesajları asenkron olarak işleyebilmesi için bir olay (event) tabanlı yapı sağlar.
+//channel.BasicAck(e.DeliveryTag,false);//RabbitMQ'da bir mesajın başarıyla işlendiğini onaylamak için kullanılır. Bu metodun amacı, mesajın işleme alındığını ve kuyruktan kaldırılmasını belirtmektir. Bu, mesajın başarılı bir şekilde işlenip işlenmediğini belirlemek için kullanılır ve mesajın tekrar işlenmeyeceğini garanti eder.
---
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+
+namespace UdemyRabbitMQ.publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://bqxqfvzg:gw3_bkaaGY-QlejPK_bAPBgA7nJJ9NDO@codfish.rmq.cloudamqp.com/bqxqfvzg");
+
+            using var connection = factory.CreateConnection();
+            var channel=connection.CreateModel();
+            //channel.QueueDeclare("hello-queue",true,false,false);//bir kuyruğu (queue) tanımlamak ve oluşturmak için kullanılan bir yöntemdir.
+
+            channel.ExchangeDeclare("logs-fanout", durable: true,type:ExchangeType.Fanout);//channel.ExchangeDeclare("logs") RabbitMQ'da bir exchange (değişim) oluşturmak için kullanılan bir komuttur. RabbitMQ, mesajları yönlendiren bir mesaj kuyruğu sistemidir ve exchange'lar bu mesajların hangi kuyruklara yönlendirileceğini belirler.
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+
+                string message = $"log {x}";
+                var messageBody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("logs-fanout", "", null, messageBody);
+
+                Console.WriteLine($"mesaj gönderilmiştir: {message}");
+            });
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
+// Kuyruğu oluşturur
+//channel.QueueDeclare(
+//    queue: queueName,    // Kuyruğun adı
+//    durable: durable,    // Kuyruğun kalıcılığı
+//    exclusive: exclusive, // Kuyruğun özel olup olmadığı
+//    autoDelete: autoDelete, // Kuyruğun otomatik silinip silinmeyeceği
+//    arguments: arguments // Kuyruğun ek argümanları
+//);
+//BasicPublish: RabbitMQ'da bir mesajı bir kuyrukta veya bir exchange'de yaymak için kullanılan yöntemdir.
+
+// Mesajı gönder
+//channel.BasicPublish(
+//    exchange: "",            // Boş exchange, doğrudan kuyruk hedeflenir
+//    routingKey: "myQueue",  // Mesajın gönderileceği kuyruk adı
+//    basicProperties: null,  // Mesajın özellikleri (null)
+//    body: body              // Gönderilecek mesajın içeriği
+//);
